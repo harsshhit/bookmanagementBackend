d55c4b013195982c070d138d89a7c423edaa81f0
Your Name <you@example.com>
2024-04-11 19:00:06 +0530

    cors fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000" }));
 
 mongoose.connect(process.env.MONGODB_URI);
 
