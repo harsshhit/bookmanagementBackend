132b255ccfb612faddcf45c1a1f9cce44c9eed97
Your Name <you@example.com>
2024-04-14 00:44:28 +0530

    nodemon

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
@@ -11,15 +9,16 @@ const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
-
 app.use(cors());
 
 mongoose.connect(process.env.MONGODB_URI);
 
-
-
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.get("/", (req, res) => {
+  res.send("Server is running"); // Example response
+});
 
 const bookRoutes = require("./routes/bookRoutes");
 app.use("/api/books", bookRoutes);
